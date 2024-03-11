1b6f8a1a254e1d5b05812ccf8dc34cf83c318034
Gregory <mitchellg2026@student.hf233.org>
2024-03-11 10:13:08 -0500

    Draft Game Abstraction

---
+++ q3-project/include/game.h
@@ -0,0 +1,11 @@
+#include "vex.h"
+
+namespace Game {
+
+    void setScore(int score);
+
+    void incrementScore();
+
+    void incrementScore(int amount);
+
+}
---
+++ q3-project/include/scoreboard.h
@@ -0,0 +1,13 @@
+#include "vex.h"
+
+namespace Scoreboard {
+
+    void displayScore(int score);
+
+    void displayHighScore(int highScore);
+
+    void incrementDisplay();
+
+    void incrementDisplay(int amount);
+
+}
